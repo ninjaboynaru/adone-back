63025f52f8de2513bd42d0d2c9c4be465666cc57
ninjaboynaru <thiagoCord755@gmail.com>
2021-10-23 16:44:41 -0400

    Added set_donation route

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, Response
+from db import *
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/set/donation/<float:amount>")
+def set_donation(amount):
+    db_error = db_set_donation(amount)
+    response = Response(status=200)
+
+    if (db_error != None):
+         response = Response(status=500)
+
+    return response
 
 app.run()
